971fa6239c9ace9d1f2592e0e468bb5daed3008b
chittiinfo <chittiinfo@gmail.com>
2022-04-12 23:06:14 +0530

    third commit

---
+++ src/main/java/com/skype/SampleApp.java
@@ -0,0 +1,13 @@
+package com.skype;
+
+/**
+ * Hello world!
+ *
+ */
+public class SampleApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
